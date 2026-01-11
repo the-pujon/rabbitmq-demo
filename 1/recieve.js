ed86b329e48ec9cb7e1469c32a20781ff0712ac6
Pujon Das <auvipujondas@gmail.com>
2026-01-11 18:23:07 +0600

    Refactor message consumer and producer to improve performance and reliability

--- 1/recieve.js
+++ 1/recieve.js
@@ -1,29 +1,105 @@
+// const amqp = require("amqplib/callback_api");
+
+// amqp.connect("amqp://localhost", (error0, connection) => {
+//   if (error0) {
+//     throw error0;
+//   }
+
+//   connection.createChannel((error1, channel) => {
+//     if (error1) {
+//       throw error1;
+//     }
+
+//     const queue = "pppp";
+
+//     // Ensure the queue exists before sending a message
+//     channel.assertQueue(queue, { durable: true });
+
+//     console.log(" [x] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+//     // Set up a consumer to receive messages from the queue
+//     channel.consume(
+//       queue,
+//       (msg) => {
+//         console.log(" [x] Received %s", msg.content.toString());
+//       },
+//       { noAck: true }
+//     );
+//   });
+// });
+
+// const amqp = require("amqplib/callback_api");
+// const startTime = Date.now();
+// amqp.connect("amqp://localhost", (error0, connection) => {
+//   if (error0) throw error0;
+
+//   connection.createChannel((error1, channel) => {
+//     if (error1) throw error1;
+
+//     const queue = "pppp";
+
+//     channel.assertQueue(queue, { durable: true });
+
+//     channel.prefetch(50);
+
+//     console.log(" [x] Waiting for messages in %s", queue);
+
+//     channel.consume(
+//       queue,
+//       (msg) => {
+//         // console.log(" [x] Received %s", msg.content.toString());
+
+//         // simulate some work if needed
+//         // setTimeout(() => {
+//         channel.ack(msg);
+//         // }, 10);
+//       },
+//       { noAck: false }
+//     );
+//     console.log("all done");
+//   });
+// });
+// const endTime = Date.now();
+// console.log("Setup time:", endTime - startTime, "ms");
+// // const amqp = require("amqplib/callback_api");
+
 const amqp = require("amqplib/callback_api");
 
+const startTime = Date.now();
+
 amqp.connect("amqp://localhost", (error0, connection) => {
-  if (error0) {
-    throw error0;
-  }
+  if (error0) throw error0;
 
   connection.createChannel((error1, channel) => {
-    if (error1) {
-      throw error1;
-    }
+    if (error1) throw error1;
 
-    const queue = "hello";
+    const queue = "pppp";
+    let receivedCount = 0;
 
-    // Ensure the queue exists before sending a message
-    channel.assertQueue(queue, { durable: false });
+    channel.assertQueue(queue, { durable: true });
+    channel.prefetch(50000);
 
-    console.log(" [x] Waiting for messages in %s. To exit press CTRL+C", queue);
-
-    // Set up a consumer to receive messages from the queue
     channel.consume(
       queue,
       (msg) => {
-        console.log(" [x] Received %s", msg.content.toString());
+        // process message
+        // channel.ack(msg);
+        receivedCount++;
+
+        if (receivedCount % 100000 === 0) {
+          channel.ack(msg, true); // ack all messages up to this one
+        }
+
+        // When all messages are received, calculate time
+        if (receivedCount === 1000000) {
+          const endTime = Date.now();
+          console.log("All messages received");
+          console.log("Consumer processing time:", endTime - startTime, "ms");
+          connection.close();
+          process.exit(0);
+        }
       },
-      { noAck: true }
+      { noAck: false }
     );
   });
 });
--- 1/send.js
+++ 1/send.js
@@ -1,32 +1,116 @@
+// const amqp = require("amqplib/callback_api");
+
+// amqp.connect("amqp://localhost", (error0, connection) => {
+//   if (error0) {
+//     throw error0;
+//   }
+
+//   connection.createChannel((error1, channel) => {
+//     if (error1) {
+//       throw error1;
+//     }
+
+//     const queue = "pppp";
+//     const msg = "Hello World!";
+
+//     // Ensure the queue exists before sending a message
+//     channel.assertQueue(queue, { durable: true });
+
+//     console.log(" [x] Sending %s", msg);
+
+//     // Send the message to the queue
+//     for (let i = 1; i <= 1000000; i++) {
+//       channel.sendToQueue(queue, Buffer.from(msg + " " + i));
+//       console.log(" [x] Sent %s", msg + " " + i);
+//     }
+//   });
+
+//   // Close the connection after a short delay to ensure the message is sent
+//   setTimeout(() => {
+//     connection.close();
+//     process.exit(0);
+//   }, 500);
+// });
+
+// const amqp = require("amqplib/callback_api");
+
+// const startTime = Date.now();
+// amqp.connect("amqp://localhost", (err, connection) => {
+//   if (err) throw err;
+
+//   connection.createConfirmChannel((err2, channel) => {
+//     if (err2) throw err2;
+
+//     const queue = "pppp";
+//     const msg = "Hello World!";
+
+//     channel.assertQueue(queue, { durable: true });
+
+//     async function publishMessages() {
+//       for (let i = 1; i <= 1000000; i++) {
+//         const ok = channel.sendToQueue(queue, Buffer.from(msg + " " + i), {
+//           persistent: true,
+//         });
+
+//         if (!ok) {
+//           await new Promise((resolve) => channel.once("drain", resolve));
+//         }
+
+//         if (i % 10000 === 0) {
+//           console.log("Sent:", i);
+//         }
+//       }
+
+//       channel.waitForConfirms(() => {
+//         console.log("All messages confirmed");
+//         connection.close();
+//         process.exit(0);
+//       });
+//     }
+
+//     publishMessages();
+//   });
+// });
+// const endTime = Date.now();
+// console.log("Setup time:", endTime - startTime, "ms");
+
 const amqp = require("amqplib/callback_api");
 
-amqp.connect("amqp://localhost", (error0, connection) => {
-  if (error0) {
-    throw error0;
-  }
+const startTime = Date.now();
 
-  connection.createChannel((error1, channel) => {
-    if (error1) {
-      throw error1;
-    }
+amqp.connect("amqp://localhost", (err, connection) => {
+  if (err) throw err;
 
-    const queue = "hello";
+  connection.createConfirmChannel((err2, channel) => {
+    if (err2) throw err2;
+
+    const queue = "pppp";
     const msg = "Hello World!";
 
-    // Ensure the queue exists before sending a message
-    channel.assertQueue(queue, { durable: false });
+    channel.assertQueue(queue, { durable: true });
 
-    console.log(" [x] Sending %s", msg);
+    async function publishMessages() {
+      for (let i = 1; i <= 1000000; i++) {
+        const ok = channel.sendToQueue(queue, Buffer.from(msg + " " + i), {
+          persistent: true,
+        });
 
-    // Send the message to the queue
-    channel.sendToQueue(queue, Buffer.from(msg));
+        if (!ok) {
+          await new Promise((resolve) => channel.once("drain", resolve));
+        }
 
-    console.log(" [x] Sent %s", msg);
+        // no console.log here to measure pure sending
+      }
+
+      channel.waitForConfirms(() => {
+        const endTime = Date.now();
+        console.log("All messages confirmed");
+        console.log("Publish time:", endTime - startTime, "ms");
+        connection.close();
+        process.exit(0);
+      });
+    }
+
+    publishMessages();
   });
-
-  // Close the connection after a short delay to ensure the message is sent
-  setTimeout(() => {
-    connection.close();
-    process.exit(0);
-  }, 500);
 });
