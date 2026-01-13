b63ee35147bbdeb9fe76505cb048f4460c31afab
Pujon Das Auvi <auvipujondas@gmail.com>
2026-01-13 23:34:35 +0600

    Refactor message handling in consumer and producer for clarity and queue setup

--- 1/recieve.js
+++ 1/recieve.js
@@ -1,32 +1,32 @@
-// const amqp = require("amqplib/callback_api");
+const amqp = require("amqplib/callback_api");
 
-// amqp.connect("amqp://localhost", (error0, connection) => {
-//   if (error0) {
-//     throw error0;
-//   }
+amqp.connect("amqp://localhost", (error0, connection) => {
+  if (error0) {
+    throw error0;
+  }
 
-//   connection.createChannel((error1, channel) => {
-//     if (error1) {
-//       throw error1;
-//     }
+  connection.createChannel((error1, channel) => {
+    if (error1) {
+      throw error1;
+    }
 
-//     const queue = "pppp";
+    const queue = "hello";
 
-//     // Ensure the queue exists before sending a message
-//     channel.assertQueue(queue, { durable: true });
+    // Ensure the queue exists before sending a message
+    channel.assertQueue(queue, { durable: false });
 
-//     console.log(" [x] Waiting for messages in %s. To exit press CTRL+C", queue);
+    console.log(" [x] Waiting for messages in %s. To exit press CTRL+C", queue);
 
-//     // Set up a consumer to receive messages from the queue
-//     channel.consume(
-//       queue,
-//       (msg) => {
-//         console.log(" [x] Received %s", msg.content.toString());
-//       },
-//       { noAck: true }
-//     );
-//   });
-// });
+    // Set up a consumer to receive messages from the queue
+    channel.consume(
+      queue,
+      (msg) => {
+        console.log(" [x] Received %s", msg.content.toString());
+      },
+      { noAck: true }
+    );
+  });
+});
 
 // const amqp = require("amqplib/callback_api");
 // const startTime = Date.now();
@@ -63,43 +63,43 @@
 // console.log("Setup time:", endTime - startTime, "ms");
 // // const amqp = require("amqplib/callback_api");
 
-const amqp = require("amqplib/callback_api");
+// const amqp = require("amqplib/callback_api");
 
-const startTime = Date.now();
+// const startTime = Date.now();
 
-amqp.connect("amqp://localhost", (error0, connection) => {
-  if (error0) throw error0;
+// amqp.connect("amqp://localhost", (error0, connection) => {
+//   if (error0) throw error0;
 
-  connection.createChannel((error1, channel) => {
-    if (error1) throw error1;
+//   connection.createChannel((error1, channel) => {
+//     if (error1) throw error1;
 
-    const queue = "pppp";
-    let receivedCount = 0;
+//     const queue = "pppp";
+//     let receivedCount = 0;
 
-    channel.assertQueue(queue, { durable: true });
-    channel.prefetch(50000);
+//     channel.assertQueue(queue, { durable: true });
+//     channel.prefetch(50000);
 
-    channel.consume(
-      queue,
-      (msg) => {
-        // process message
-        // channel.ack(msg);
-        receivedCount++;
+//     channel.consume(
+//       queue,
+//       (msg) => {
+//         // process message
+//         // channel.ack(msg);
+//         receivedCount++;
 
-        if (receivedCount % 100000 === 0) {
-          channel.ack(msg, true); // ack all messages up to this one
-        }
+//         if (receivedCount % 100000 === 0) {
+//           channel.ack(msg, true); // ack all messages up to this one
+//         }
 
-        // When all messages are received, calculate time
-        if (receivedCount === 1000000) {
-          const endTime = Date.now();
-          console.log("All messages received");
-          console.log("Consumer processing time:", endTime - startTime, "ms");
-          connection.close();
-          process.exit(0);
-        }
-      },
-      { noAck: false }
-    );
-  });
-});
+//         // When all messages are received, calculate time
+//         if (receivedCount === 1000000) {
+//           const endTime = Date.now();
+//           console.log("All messages received");
+//           console.log("Consumer processing time:", endTime - startTime, "ms");
+//           connection.close();
+//           process.exit(0);
+//         }
+//       },
+//       { noAck: false }
+//     );
+//   });
+// });
--- 1/send.js
+++ 1/send.js
@@ -1,36 +1,40 @@
-// const amqp = require("amqplib/callback_api");
+const amqp = require("amqplib/callback_api");
 
-// amqp.connect("amqp://localhost", (error0, connection) => {
-//   if (error0) {
-//     throw error0;
-//   }
+amqp.connect("amqp://localhost", (error0, connection) => {
+  if (error0) {
+    throw error0;
+  }
 
-//   connection.createChannel((error1, channel) => {
-//     if (error1) {
-//       throw error1;
-//     }
+  connection.createChannel((error1, channel) => {
+    if (error1) {
+      throw error1;
+    }
 
-//     const queue = "pppp";
-//     const msg = "Hello World!";
+    const queue = "hello";
+    const msg = "Hello World!";
 
-//     // Ensure the queue exists before sending a message
-//     channel.assertQueue(queue, { durable: true });
+    // Ensure the queue exists before sending a message
+    channel.assertQueue(queue, { durable: false });
 
-//     console.log(" [x] Sending %s", msg);
+    console.log(" [x] Sending %s", msg);
 
-//     // Send the message to the queue
-//     for (let i = 1; i <= 1000000; i++) {
-//       channel.sendToQueue(queue, Buffer.from(msg + " " + i));
-//       console.log(" [x] Sent %s", msg + " " + i);
-//     }
-//   });
+    channel.sendToQueue(queue, Buffer.from(msg));
 
-//   // Close the connection after a short delay to ensure the message is sent
-//   setTimeout(() => {
-//     connection.close();
-//     process.exit(0);
-//   }, 500);
-// });
+    console.log(" [x] msg send");
+
+    // Send the message to the queue
+    // for (let i = 1; i <= 1000000; i++) {
+
+    //   console.log(" [x] Sent %s", msg + " " + i);
+    // }
+  });
+
+  // Close the connection after a short delay to ensure the message is sent
+  setTimeout(() => {
+    connection.close();
+    process.exit(0);
+  }, 500);
+});
 
 // const amqp = require("amqplib/callback_api");
 
@@ -74,43 +78,43 @@
 // const endTime = Date.now();
 // console.log("Setup time:", endTime - startTime, "ms");
 
-const amqp = require("amqplib/callback_api");
+// const amqp = require("amqplib/callback_api");
 
-const startTime = Date.now();
+// const startTime = Date.now();
 
-amqp.connect("amqp://localhost", (err, connection) => {
-  if (err) throw err;
+// amqp.connect("amqp://localhost", (err, connection) => {
+//   if (err) throw err;
 
-  connection.createConfirmChannel((err2, channel) => {
-    if (err2) throw err2;
+//   connection.createConfirmChannel((err2, channel) => {
+//     if (err2) throw err2;
 
-    const queue = "pppp";
-    const msg = "Hello World!";
+//     const queue = "pppp";
+//     const msg = "Hello World!";
 
-    channel.assertQueue(queue, { durable: true });
+//     channel.assertQueue(queue, { durable: true });
 
-    async function publishMessages() {
-      for (let i = 1; i <= 1000000; i++) {
-        const ok = channel.sendToQueue(queue, Buffer.from(msg + " " + i), {
-          persistent: true,
-        });
+//     async function publishMessages() {
+//       for (let i = 1; i <= 1000000; i++) {
+//         const ok = channel.sendToQueue(queue, Buffer.from(msg + " " + i), {
+//           persistent: true,
+//         });
 
-        if (!ok) {
-          await new Promise((resolve) => channel.once("drain", resolve));
-        }
+//         if (!ok) {
+//           await new Promise((resolve) => channel.once("drain", resolve));
+//         }
 
-        // no console.log here to measure pure sending
-      }
+//         // no console.log here to measure pure sending
+//       }
 
-      channel.waitForConfirms(() => {
-        const endTime = Date.now();
-        console.log("All messages confirmed");
-        console.log("Publish time:", endTime - startTime, "ms");
-        connection.close();
-        process.exit(0);
-      });
-    }
+//       channel.waitForConfirms(() => {
+//         const endTime = Date.now();
+//         console.log("All messages confirmed");
+//         console.log("Publish time:", endTime - startTime, "ms");
+//         connection.close();
+//         process.exit(0);
+//       });
+//     }
 
-    publishMessages();
-  });
-});
+//     publishMessages();
+//   });
+// });
